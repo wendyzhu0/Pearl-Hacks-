ff6c0b5fd255c099e9b9dd32162772011003323d
wendyzhu0415 <WendyZhu2002@outlook.com>
2022-02-19 18:01:47 -0500

    Finish first function

---
+++ ask_for_time.py
@@ -0,0 +1,17 @@
+"""ask for input for avaliable time slots with start time and end time"""
+
+
+def avaliable_time_slot() -> list[float]:
+    i: int = 1
+    ls: list[float] = list()
+    while i < 1000:
+        print(f"====Please enter your {i}th avaliable timeslot====")
+        start_time: float = input("Please enter the start time: ")
+        end_time: float = input("Please enter the end time: ")
+        ls.append(start_time)
+        ls.append(end_time)
+        ask: str = input("Is that all?(Y/N)")
+        if ask == "Y":
+            return ls
+        else:
+            i += 1
